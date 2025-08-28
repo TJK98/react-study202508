72109b01ce780ad3e85e0c322102ef8ec5f218fe
TJK98 <wtj1998@naver.com>
2025-08-28 09:25:13 +0900

    실습문제 2-3. 동적 리스트 렌더링 - `TodoItem` 컴포넌트에 props 추가 및 동적 클래스 바인딩 수정 - 완료 여부에 따라 스타일 동적 적용 - `TodoMain` 컴포넌트에서 todo 리스트를 map 함수로 렌더링하도록 변경 - `DUMMY_TODOS` 샘플 데이터 생성 및 연동

---
+++ src/components/Todos/dummy-data/DUMMY-TODOS.js
@@ -0,0 +1,6 @@
+export const TODOS = [
+    { id: 1, title: '리액트 학습', done: false },
+    { id: 2, title: '점심 먹기', done: true },
+    { id: 3, title: '프로젝트 연구하기', done: true },
+    { id: 4, title: '숙제하기', done: false },
+];
